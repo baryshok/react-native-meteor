8915b463b84a4acf6bdcfec4e5161a05363ea47c
baryshok <baryshok@gmail.com>
2017-01-17 02:16:05 +0300

    Remove calling this.connect() from logout callback

--- src/user/User.js
+++ src/user/User.js
@@ -25,7 +25,6 @@ module.exports = {
   logout(callback) {
     call("logout", err => {
       this.handleLogout();
-      this.connect();
 
       typeof callback == 'function' && callback(err);
     });
